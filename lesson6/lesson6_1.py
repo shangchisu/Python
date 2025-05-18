d89d2294978a5317c2f914aecf18afde106216b3
蘇上祺 <108869704+shangchisu@users.noreply.github.com>
2025-05-18 08:38:43 +0000

    lesson6_1_3

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -1,4 +1,4 @@
-if __name__ == "__main__":
+def main():
     try:
         hight: int = int (input("請輸入您的身高(120~220cm間)"))
         if hight < 120 or hight > 220:
@@ -26,3 +26,6 @@ if __name__ == "__main__":
         else:
             print("正常，太棒了!")
     print("應用程式結束")
+#內建變數
+if __name__ == "__main__":
+    main()
